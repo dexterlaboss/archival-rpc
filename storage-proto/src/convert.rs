d0b3fd11fc9d9b71127acf487a6f373f6fced774
aurimas <aurimas.n@dexterlab.com>
2024-08-25 19:24:06 +0300

    Fix ui_amount value to match original solana validator rpc

--- storage-proto/src/convert.rs
+++ storage-proto/src/convert.rs
@@ -595,19 +595,29 @@ impl From<generated::TokenBalance> for TransactionTokenBalance {
     fn from(value: generated::TokenBalance) -> Self {
         let ui_token_amount = value.ui_token_amount.unwrap_or_default();
         let ui_amount_float = ui_token_amount.ui_amount_string.parse::<f64>().ok();
+
+        let ui_amount = match ui_amount_float {
+            Some(parsed_float) => {
+                if (parsed_float - f64::default()).abs() > f64::EPSILON {
+                    Some(parsed_float)
+                } else {
+                    None
+                }
+            }
+            None => {
+                if (ui_token_amount.ui_amount - f64::default()).abs() > f64::EPSILON {
+                    Some(ui_token_amount.ui_amount)
+                } else {
+                    None
+                }
+            }
+        };
+
         Self {
             account_index: value.account_index as u8,
             mint: value.mint,
             ui_token_amount: UiTokenAmount {
-                ui_amount: if let Some(parsed_float) = ui_amount_float {
-                    Some(parsed_float)
-                } else {
-                    if (ui_token_amount.ui_amount - f64::default()).abs() > f64::EPSILON {
-                        Some(ui_token_amount.ui_amount)
-                    } else {
-                        None
-                    }
-                },
+                ui_amount,
                 decimals: ui_token_amount.decimals as u8,
                 amount: ui_token_amount.amount.clone(),
                 ui_amount_string: if !ui_token_amount.ui_amount_string.is_empty() {
